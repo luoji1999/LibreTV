48df2d0b2dee1e6b030e3366f13bf36a657a389b
luoji1999 <1357780119@qq.com>
2025-09-08 22:28:15 +0800

    Update customer_site.js

--- js/customer_site.js
+++ js/customer_site.js
@@ -1,8 +1,28 @@
 const CUSTOMER_SITES = {
+    // 现有源
     qiqi: {
         api: 'https://www.qiqidys.com/api.php/provide/vod',
         name: '七七资源',
-    }
+    },
+    
+    // 添加新源
+    newsource1: {
+        api: 'http://caiji.dyttzyapi.com/api.php/provide/vod',
+        name: '电影天堂资源',
+    },
+    
+    newsource2: {
+        api: 'https://bfzyapi.com/api.php/provide/vod',
+        name: '暴风资源',
+    },
+    newsource3: {
+        api: 'https://cj.rycjapi.com/api.php/provide/vod',
+        name: '如意资源',
+    },
+    newsource4: {
+        api: 'https://tyyszy.com/api.php/provide/vod',
+        name: '天涯资源',
+    },
 };
 
 // 调用全局方法合并
